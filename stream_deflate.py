588f5654a39ce46bab82909e22e608763a528cf1
Michal Charemza <michal@charemza.name>
2021-10-16 15:00:46 +0100

    feat: initial skeleton

---
+++ stream_deflate.py
@@ -0,0 +1,3 @@
+
+def stream_deflate():
+    pass
---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from stream_deflate import stream_deflate
+
+
+class TestStreamDeflate(unittest.TestCase):
+
+    def test_stream_deflate(self):
+        stream_deflate()
