d28f121f36e159c3267af006923ed788c5274480
Michal Charemza <michal@charemza.name>
2021-10-16 22:32:21 +0100

    feat: process uncompressed blocks

--- stream_deflate.py
+++ stream_deflate.py
@@ -1,3 +1,78 @@
+from struct import Struct
 
-def stream_deflate():
+
+def stream_deflate(deflate_chunks, chunk_size=65536):
+    b_len_struct = Struct('<H')
+
+    def get_readers(iterable):
+        chunk = b''
+        offset_byte = 0
+        offset_bit = 0
+        it = iter(iterable)
+
+        def _next_or_truncated_error():
+            try:
+                return next(it)
+            except StopIteration:
+                raise TruncatedDataError from None
+
+        def _get_bits(num_bits):
+            nonlocal chunk, offset_byte, offset_bit
+
+            out = bytearray(-(-num_bits // 8))
+            out_offset_bit = 0
+
+            while num_bits:
+                if offset_bit == 8:
+                    offset_bit = 0
+                    offset_byte += 1
+
+                if offset_byte == len(chunk):
+                    chunk = _next_or_truncated_error()
+                    offset_byte = 0
+
+                out[out_offset_bit // 8] |= chunk[offset_byte] & (2 ** offset_bit)
+
+                num_bits -= 1
+                offset_bit += 1
+                out_offset_bit += 1
+
+            return bytes(out)
+
+        def _yield_bytes(num_bytes):
+            nonlocal chunk, offset_byte, offset_bit
+
+            if offset_bit:
+                offset_byte += 1
+            offset_bit = 0
+
+            while num_bytes:
+                if offset_byte == len(chunk):
+                    chunk = _next_or_truncated_error()
+                    offset_byte = 0
+                to_yield = min(num_bytes, len(chunk) - offset_byte, chunk_size)
+                offset_byte += to_yield
+                num_bytes -= to_yield
+                yield chunk[offset_byte - to_yield:offset_byte]
+
+        def _get_bytes(num_bytes):
+            return b''.join(_yield_bytes(num_bytes))
+
+        return _get_bits, _get_bytes, _yield_bytes
+
+    def upcompressed(get_bits, get_bytes, yield_bytes):
+        b_final = b'\0'
+
+        while not b_final[0]:
+            b_final = get_bits(1)
+            b_type = get_bits(2)
+            b_len, = b_len_struct.unpack(get_bytes(2))
+            get_bytes(2)
+            yield from yield_bytes(b_len)
+
+    get_bits, get_bytes, yield_bytes = get_readers(deflate_chunks)
+    yield from upcompressed(get_bits, get_bytes, yield_bytes)
+
+
+class TruncatedDataError():
     pass
--- test.py
+++ test.py
@@ -1,9 +1,37 @@
+import itertools
 import unittest
+import zlib
+from struct import Struct
 
 from stream_deflate import stream_deflate
 
 
 class TestStreamDeflate(unittest.TestCase):
 
-    def test_stream_deflate(self):
-        stream_deflate()
+    def test_uncompressed_block(self):
+        b_len_struct = Struct('<H')
+        data = b'Some uncompressed bytes'
+        stream = \
+            b'\0' + \
+            Struct('<H').pack(len(data)) + \
+            Struct('<H').pack(65535 - len(data)) + \
+            data + \
+            b'\1' + \
+            Struct('<H').pack(len(data)) + \
+            Struct('<H').pack(65535 - len(data)) + \
+            data
+
+        input_sizes = [1, 7, 65536]
+        output_sizes = [1, 7, 65536]
+
+        def content(input_size):
+            for i in range(0, len(stream), input_size):
+                yield stream[i:i + input_size]
+
+        # Make sure it really is DEFLATEd
+        self.assertEqual(zlib.decompress(stream, wbits=-zlib.MAX_WBITS), data * 2)
+
+        for input_size, output_size in itertools.product(input_sizes, output_sizes):
+            with self.subTest(input_size=input_size, output_size=output_size):
+                uncompressed = b''.join(stream_deflate(content(input_size), chunk_size=output_size))
+                self.assertEqual(uncompressed, data * 2)
