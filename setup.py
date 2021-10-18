1967dc4e1e49dc331fc029fd1e044f87322694eb
Michal Charemza <michal@charemza.name>
2021-10-18 21:27:19 +0100

    build(release): v0.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='stream-inflate',
-    version='0.0.0',
+    version='0.0.1',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Uncompress DEFLATE streams in pure Python',
