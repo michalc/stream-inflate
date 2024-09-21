2ca9b58af8a81c35c8e964389ac2342847644d32
Michal Charemza <michal@charemza.name>
2024-09-21 11:58:53 +0100

    ci: fix code coverage off-by-two errors
    The slightly hack of putting in comments into the code made the code coverage
    reports off-by-two in terms of where the lines get covered or not

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
+import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
+macros, compiler_directives = \
+    ([('CYTHON_TRACE', '1')], {'linetrace': True}) if os.environ.get('STREAM_INFLATE_CODE_COVERAGE') == '1' else \
+    ([], {})
+
+print('Building...')
+print('macros:', macros)
+print('compiler_directives:', compiler_directives)
+
 setup(
-    ext_modules=cythonize("stream_inflate.py"),
+    ext_modules=cythonize([Extension(
+        "stream_inflate",
+        ["stream_inflate.py"],
+        define_macros=macros,
+    )], compiler_directives=compiler_directives),
     # Python 3.6 seems to need all of the below, even though they are in pyproject.toml
     name="stream-inflate",
     version="0.0.0.dev0",
